1be3a4e491500abcc8e4a7d51b245af33e0d3462
bcis93 <bryancisneros93@gmail.com>
2020-05-01 13:35:34 -0600

    Adding decoding state machine

--- decode.c
+++ decode.c
@@ -8,11 +8,25 @@
 #define DOT_MIN (DOT_LENGTH - 1)
 #define DOT_MAX (DOT_LENGTH + 1)
 #define DASH_LENGTH (DOT_LENGTH * 3)
-#define DASH_MIN (DASH_LENGTH - 1)
-#define DASH_MAX (DASH_LENGTH + 1)
+#define DASH_MIN (DASH_LENGTH - 2)
+#define DASH_MAX (DASH_LENGTH + 2)
+
+#define SYMBOL_SEPARATOR_LENGTH (DOT_LENGTH)
+#define SYMBOL_SEPARATOR_MIN (DOT_MIN)
+#define SYMBOL_SEPARATOR_MAX (DOT_MAX)
+
+#define CHARACTER_SEPARATOR_LENGTH (DASH_LENGTH)
+#define CHARACTER_SEPARATOR_MIN (DASH_MIN)
+#define CHARACTER_SEPARATOR_MAX (DASH_MAX)
+
+#define WORD_SEPARATOR_LENGTH (DOT_LENGTH * 7)
+#define WORD_SEPARATOR_MIN (WORD_SEPARATOR_LENGTH - 3)
+#define WORD_SEPARATOR_MAX (WORD_SEPARATOR_LENGTH + 3)
 
 #define MAX_MORSE_SEQUENCE (6) // The longest sequence to determine a character is 5, + 1 for 'morse_letter_end' after
 #define NUMBER_OF_CHARACTERS (36) // 26 letters + 10 numbers
+
+#define MAX_ON_OFF_LENGTH (7) // the longest on/off sequence is 7 (signifying a space between words)
 
 typedef enum
 {
@@ -20,8 +34,16 @@ typedef enum
     morse_dot,
     morse_dash,
     morse_letter_end,
-    morse_space,
+    morse_word_end,
 } morse_t;
+
+typedef enum
+{
+    state_idle,
+    state_start,
+    state_on,
+    state_off,
+} state_t;
 
 typedef struct
 {
@@ -71,6 +93,17 @@ const character_t characters[NUMBER_OF_CHARACTERS] =
 };
 
 morse_t current_sequence[MAX_MORSE_SEQUENCE] = {};
+bool on_off_buffer[MAX_ON_OFF_LENGTH] = {};
+
+state_t current_state = state_idle;
+
+static void reset_on_off_buffer(void)
+{
+    for (uint8_t i = 0; i < MAX_ON_OFF_LENGTH; i++)
+    {
+        on_off_buffer[i] = morse_empty;
+    }
+}
 
 static void reset_current_sequence(void)
 {
@@ -98,8 +131,8 @@ static char compare_sequence(void)
         }
 
         // If the character matched the sequence, and the next morse symbol in
-        // the sequence is morse_letter_end, we found a match
-        if (character_match && (current_sequence[len] == morse_letter_end))
+        // the sequence is morse_letter_end or morse_word_end, we found a match
+        if (character_match && (current_sequence[len] == morse_letter_end || current_sequence[len] == morse_word_end))
         {
             // we found a match! Return the char
             return characters[i].name;
@@ -127,26 +160,126 @@ void decode_init(void)
 
 void decode_run(bool light_on)
 {
-    // State machine has not been developed yet...
-    // Right now, this just simulates that we just received the 'letter end'
-    // code, meaning we need to try to match the received sequence to a letter.
-    // We have initialized the sequence to an invalid character, which should be
-    // the worst case for the comparisons
-    morse_t latest_symbol = morse_letter_end;
-    
-    if (latest_symbol == morse_letter_end)
-    {
-        char c = compare_sequence();
-        if (c)
-        {
-            printf("%c\n", c);
-        }
-        else
-        {
-            printf("No match!\n");
-        }
+    static uint8_t on_count = 0;
+    static uint8_t off_count = 0;
+    morse_t latest_symbol = morse_empty;
 
-        reset_current_sequence();
+    // Update on/off count
+    if (light_on)
+    {
+        on_count++;
     }
+    else
+    {
+        off_count++;
+    }
+    
+
+    // State Action
+    switch (current_state)
+    {
+    case state_idle:
+        break;
+    case state_start:
+        break;
+    case state_on:
+        break;
+    case state_off:
+        break;
+    default:
+        break;
+    }
+
+    // State Update
+    switch (current_state)
+    {
+    case state_idle:
+        if (!light_on)
+        {
+            current_state = state_start;
+        }
+        break;
+    case state_start:
+        if (light_on)
+        {
+            current_state = state_on;
+            on_count = 1; // reset to 1 because have seen 1 ON so far
+        }
+        break;
+    case state_on:
+        if (on_count > DASH_MAX) // If the light has gone back to an idle state
+        {
+            current_state = state_idle;
+        }
+        else if (!light_on)
+        {
+            if (on_count >= DOT_MIN && on_count <= DOT_MAX)
+            {
+                // A dot was detected!
+                latest_symbol = morse_dot;
+            }
+            else if (on_count >= DASH_MIN && on_count <= DASH_MAX)
+            {
+                // A dash was detected!
+                latest_symbol = morse_dash;
+            }
+
+            current_state = state_off;
+            off_count = 1; // reset to 1 because have seen 1 OFF so far
+        }
+        break;
+    case state_off:
+        if (light_on)
+        {
+            if (off_count < SYMBOL_SEPARATOR_MAX)
+            {
+                // next symbol, nothing to do...
+            }
+            else if (off_count >= CHARACTER_SEPARATOR_MIN && off_count <= CHARACTER_SEPARATOR_MAX)
+            {
+                // end of character detected!
+                latest_symbol = morse_letter_end;
+            }
+            else if (off_count > WORD_SEPARATOR_MIN)
+            {
+                // end of word detected!
+                latest_symbol = morse_word_end;
+            }
+
+            current_state = state_on;
+            on_count = 1; // reset to 1 because have seen 1 ON so far
+        }
+        break;
+    default:
+        break;
+    }
+
+
+    
+    if (latest_symbol != morse_empty)
+    {
+        if (latest_symbol == morse_letter_end || latest_symbol == morse_word_end)
+        {
+            char c = compare_sequence();
+            if (c)
+            {
+                if (latest_symbol == morse_word_end)
+                {
+                    printf("%c ", c); // print the character plus a space
+                }
+                else
+                {
+                    printf("%c", c); // just print the character
+                }
+            }
+            else
+            {
+                printf("\nNo match!\n");
+            }
+
+            reset_current_sequence();
+        }
+    }
+    
 
 }
